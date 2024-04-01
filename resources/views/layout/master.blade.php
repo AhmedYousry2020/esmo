3c40e946838609de676e99dff1a099c6f5d82fad
Ahmed Yousry <63677504+AhmedYousry2020@users.noreply.github.com>
2024-04-01 12:58:04 +0200

    updates

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Home 01</title>
+	<title>                                شركة انظمة الكفاءة للتشغيل والصيانة
+	</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
